594974d826b01915b374c75d7cf1144b671b5737
OwlBe <iamaowlbe@gmail.com>
2026-09-06 03:32:32 -0400

    needless since PermissionHolderEvent handles getNode() & getTarget()

--- src/main/java/owlbe/skriptLuckPerms/luckperms/bukkitevents/GroupRemoveEvent.java
+++ src/main/java/owlbe/skriptLuckPerms/luckperms/bukkitevents/GroupRemoveEvent.java
@@ -9,23 +9,10 @@ import owlbe.skriptLuckPerms.luckperms.bukkitevents.parent.PermissionHolderEvent
 public class GroupRemoveEvent extends PermissionHolderEvent {
 
 	private static final HandlerList HANDLER_LIST = new HandlerList();
-	private final PermissionHolder holder;
-	private final InheritanceNode node;
 
 	public GroupRemoveEvent(PermissionHolder holder, InheritanceNode node) {
 		super(holder, node);
-		this.holder = holder;
-		this.node = node;
 	}
-
-	public @NotNull InheritanceNode getNode() {
-		return this.node;
-	}
-
-	public @NotNull PermissionHolder getTarget() {
-		return this.holder;
-	}
-
 	public static HandlerList getHandlerList() {
 		return HANDLER_LIST;
 	}
